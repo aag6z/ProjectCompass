be514f39029d6b8da87d9dbb8a1caf3f1bd6517c
AmyG <amygetc@gmail.com>
2014-11-19 21:40:04 -0500

    changes

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    $("#a").hide();
+    $("#showa").click(function(){
+    	$("#a").show( 2000 );
+    });
+    $("#b").hide();
+    
+function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      	}
+      	google.maps.event.addDomListener(window, 'load', initialize);
+    	
+
+
+
+
+});
